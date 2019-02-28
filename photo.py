e89770a342704ca2e8251f22be5beb44481f2ab3
eveningkid <arn.dellinger@gmail.com>
2019-02-28 19:26:52 +0100

    Update photo model

--- photo.py
+++ photo.py
@@ -3,3 +3,31 @@ class Photo:
     self.is_vertical = is_vertical
     self.tags = tags
     self.nb_tags = len(tags)
+
+  def calculate_point(self, other_photo):
+    return min(
+      self.common_tags(other_photo),
+      self.tags_not_in_other(other_photo),
+      self.other_tags_not_in_ours(other_photo)
+    ) 
+  
+  def common_tags(self, other_photo):
+    tags = set()
+    for tag in other_photo.tags:
+      if tag in self.tags:
+        tags.add(tag)
+    return len(tags)
+  
+  def tags_not_in_other(self, other_photo):
+    tags = set()
+    for tag in self.tags:
+      if tag not in other_photo.tags:
+        tags.add(tag)
+    return len(tags)
+  
+  def other_tags_not_in_ours(self, other_photo):
+    tags = set()
+    for tag in other_photo.tags:
+      if tag not in self.tags:
+        tags.add(tag)
+    return len(tags)
