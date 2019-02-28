382aabdac43919a073035f00fad0de60cd426870
eveningkid <arn.dellinger@gmail.com>
2019-02-28 19:30:22 +0100

    Update main

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
-from parser import parser
+from Parser import parser
 
 DATASET_PATH = os.path.join(os.getcwd(), 'dataset')
 
 for file_path in os.listdir(DATASET_PATH):
-  with open(os.path.join(DATASET_PATH, file_path)) as f:
-    lines = f.readlines()
-    photos = parser(lines)
-    print(photos)
-  
+  dataset_file_path = os.path.join(DATASET_PATH, file_path)
+  photos = parser(dataset_file_path)
+  print(photos)
+
   # For testing only, breaks the loop
   break
