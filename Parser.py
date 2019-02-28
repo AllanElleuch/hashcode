cb69680554a1ce110c7152c5d27d7062590a5b98
Allan Elleuch <allan.elleuch@gmail.com>
2019-02-28 20:18:39 +0100

    update parser

--- Parser.py
+++ Parser.py
@@ -2,19 +2,22 @@ from photo import Photo
 
 def parser(file):
     photos = []
+    id=0
     with open(file, 'r') as f:
         for i, line in enumerate(f):
             if i == 0: nbRow = line.split(' ')
             else:
+                
                 line = line.rstrip('\r\n')
                 print(line.split(' '))
                 orientation,numberTag, *tags =  line.split(' ') #orientation = V/H
                 isvertical = True if orientation=="V" else False
-                photo = Photo(isvertical,numberTag,tags)
+                photo = Photo(id,isvertical,numberTag,tags)
+                id++
                 #print(photo)
                 photos.append(photo) #replace with constructor of a photo
     return photos #rides, int(rows), int(cols), int(n_vehicles), int(bonus), int(t)
 
 #to test
-#parser("./a_example.txt")
+parser("./a_example.txt")
 
