ede818be77f1a3ebb66d657ebe48ae0a9084c97a
Allan Elleuch <allan.elleuch@gmail.com>
2019-02-28 19:37:24 +0100

    accept commit

--- photo.py
+++ photo.py
@@ -5,7 +5,7 @@ class Photo:
     self.nb_tags = len(tags)
 
   def __repr__(self):
-        return "is vertical : " + str(self.is_vertical) +" nbtags : " + self.nb_tags + " tags : ".join(self.tags)  
+        return "is vertical : " + str(self.is_vertical) +" nbtags : " + str(self.nb_tags) + " tags : ".join(self.tags)  +"\n"
 
 
   def calculate_point(self, other_photo):
