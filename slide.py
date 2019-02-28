63bb5f989bd0fd183de1d5d84f91a29bed912c9f
eveningkid <arn.dellinger@gmail.com>
2019-02-28 20:18:08 +0100

    Add identifier

--- slide.py
+++ slide.py
@@ -2,10 +2,17 @@ from photo import Photo
 
 class Slide:
   def __init__(self, is_vertical, photos):
+    self.id = self.compute_id()
     self.is_vertical = is_vertical
     self.photos = photos
     self.tags = self.compute_tags()
   
+  def compute_id(self):
+    ids = []
+    for photo in self.photos:
+      ids.append(photo.id)
+    return ' '.join(ids)
+
   def compute_tags(self):
     tags = set()
     for photo in self.photos:
@@ -14,6 +21,6 @@ class Slide:
     return tags
 
   def calculate_point(self, other_slide):
-    photo = Photo(self.is_vertical, len(self.tags), self.tags)
-    other_photo = Photo(other_slide.is_vertical, len(other_slide.tags), other_slide.tags)
+    photo = Photo(self.id, self.is_vertical, len(self.tags), self.tags)
+    other_photo = Photo(other_slide.id, other_slide.is_vertical, len(other_slide.tags), other_slide.tags)
     return photo.calculate_point(other_photo)
